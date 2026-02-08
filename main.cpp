d3c936f310f4fcaa93d235e41915a11edca1c317
Nikodem Nowakowski <radkon12345@gmail.com>
2026-02-08 18:30:33 +0100

    Added clang-tidy and cmake.

--- main.cpp
+++ main.cpp
@@ -1,58 +1,51 @@
 #include <iostream>
-#include <fstream>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-
 struct Pixel {
-    unsigned char red, green, blue;
+  unsigned char red, green, blue;
 };
-
 
 struct Image {
-    int width;
-    int height;
-    std::vector<Pixel> pixels;
+  int width;
+  int height;
+  std::vector<Pixel> pixels;
 
-    Pixel get_pixel(int x, int y) {
-        return pixels[y * width + x];
-    }
+  Pixel get_pixel(int x, int y) { return pixels[y * width + x]; }
 };
 
-
 Image load_jpg_image(std::string filename) {
-    int width, height, channels;
-    unsigned char *img_data = stbi_load(filename.c_str(), &width, &height, &channels, 3);
-    if (img_data == NULL) {
-        return Image{0, 0, {}};
-    }
-    Image loaded_image;
-    loaded_image.width = width;
-    loaded_image.height = height;
-    loaded_image.pixels.resize(width * height);
+  int width, height, channels;
+  unsigned char *img_data =
+      stbi_load(filename.c_str(), &width, &height, &channels, 3);
+  if (img_data == NULL) {
+    return Image{0, 0, {}};
+  }
+  Image loaded_image;
+  loaded_image.width = width;
+  loaded_image.height = height;
+  loaded_image.pixels.resize(width * height);
 
-    for (int i = 0, j = 0; i < width * height * 3; i += 3) {
-        Pixel pixel{
-            img_data[i], // R
-            img_data[i + 1], // G
-            img_data[i + 2] // B
-            };
-        loaded_image.pixels[j] = pixel;
-        j++;
-    }
-    stbi_image_free(img_data);
-    return loaded_image;
+  for (int i = 0, j = 0; i < width * height * 3; i += 3) {
+    Pixel pixel{
+        img_data[i],     // R
+        img_data[i + 1], // G
+        img_data[i + 2]  // B
+    };
+    loaded_image.pixels[j] = pixel;
+    j++;
+  }
+  stbi_image_free(img_data);
+  return loaded_image;
 }
 
-
-
-
 int main(void) {
-    std::string file_name = "test.jpg";
-    Image image = load_jpg_image(file_name);
-    for (Pixel &pixel : image.pixels) {
-        std::cout << "R: " << (int)pixel.red << "G: " << (int)pixel.green << "B: " << (int)pixel.blue << std::endl;
-    }
+  std::string file_name = "test.jpg";
+  Image image = load_jpg_image(file_name);
+  for (Pixel &pixel : image.pixels) {
+    std::cout << (int)pixel.red << (int)pixel.green << (int)pixel.blue
+              << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
